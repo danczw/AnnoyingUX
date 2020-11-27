6dee6840d1c6152d798592f99a61980ba368b41b
danczw <devdancz@pm.me>
2020-11-28 00:18:46 +0100

    Added first huntable bouncing ball

--- interact.js
+++ interact.js
@@ -1,34 +1,72 @@
-const startButton = d3.select(".startButton");
+const svg1selector = document.querySelector('.svg1');
+const svgRect = svg1selector.getBoundingClientRect();
+const svgWidth = svgRect.width;
+const svgHeight = svgRect.height;
+buttonX = svgWidth / 2
+buttonY = svgHeight / 2
 
-const buttonShadow = startButton.append("ellipse")
-    .attr("cx", 75)
-    .attr("cy", 130)
-    .attr("rx", 35)
-    .attr("ry", 10)
-    .attr('fill', 'url(#gradient)');
+const svg1 = d3.select('.svg1');
 
-// Define the gradient
-var gradient = startButton.append("startButton:defs")
-    .append("startButton:radialGradient")
-    .attr("id", "gradient")
-    .attr("x1", "0%")
-    .attr("y1", "0%")
-    .attr("x2", "100%")
-    .attr("y2", "100%")
-    .attr("spreadMethod", "pad");
+// Define start button
+const button = svg1.append('circle')
+    .attr('cx', buttonX)
+    .attr('cy', buttonY)
+    .attr('r', 28)
+    .style('fill', '#323031');
 
-// Define the gradient colors
-gradient.append("startButton:stop")
-    .attr("offset", "0%")
-    .attr("stop-color", "#323031")
-    .attr("stop-opacity", 0.6);
-gradient.append("startButton:stop")
-    .attr("offset", "100%")
-    .attr("stop-color", "#323031")
-    .attr("stop-opacity", 0);
+// Define the shadow and gradient
+const buttonShadow = svg1.append('ellipse')
+    .attr('cx', buttonX)
+    .attr('cy', buttonY + 40)
+    .attr('rx', 35)
+    .attr('ry', 10)
+    .attr('fill', 'url(#shadowGradient)');
+var shadowGradient = svg1.append('svg1:defs')
+    .append('svg1:radialGradient')
+    .attr('id', 'shadowGradient')
+    .attr('x1', '0%')
+    .attr('y1', '0%')
+    .attr('x2', '100%')
+    .attr('y2', '100%')
+    .attr('spreadMethod', 'pad');
+shadowGradient.append('svg1:stop')
+    .attr('offset', '0%')
+    .attr('stop-color', '#323031')
+    .attr('stop-opacity', 0.6);
+shadowGradient.append('svg1:stop')
+    .attr('offset', '100%')
+    .attr('stop-color', '#323031')
+    .attr('stop-opacity', 0);
 
-const button = startButton.append("circle")
-    .attr("cx", 75)
-    .attr("cy", 75)
-    .attr("r", 40)
-    .style("fill", "#323031");
+// Start button interaction
+d3.selectAll("circle")
+    .on("mouseover", function() {
+        buttonX = svgWidth * Math.random()
+        buttonY = svgHeight * Math.random()
+        button.transition()
+            .duration(0)
+            .attr('cx', buttonX)
+            .attr('cy', buttonY)
+        buttonShadow.transition()
+            .duration(0)
+            .attr('cx', buttonX)
+            .attr('cy', buttonY + 40)
+        bounceUp();
+    });
+
+// Loop bouncing button
+function bounceUp() {
+    button.transition()
+        .duration(1500)
+        .attr('cy', buttonY - 70)
+        .ease(d3.easeCubic)
+        .on('end', bounceDown)
+};
+function bounceDown() {
+    button.transition()
+        .duration(1500)
+        .attr('cy', buttonY)
+        .ease(d3.easeCubic)
+        .on('end', bounceUp)
+};
+bounceUp();
